3593c248ce0f00cc81052b19e614fb9056e6a902
LeeEoJin <LeeEoJin@Zetstream>
2015-11-04 09:46:27 +0900

    달리기 실습과제

---
+++ JavaPractice/src/org/dimigo/thread/Race.java
@@ -0,0 +1,22 @@
+package org.dimigo.thread;
+
+public class Race {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("main thread start");
+		
+		Thread t1 = new Runner("방현우");
+		Thread t2 = new Runner("김현우");
+		
+		t2.setPriority(Thread.MAX_PRIORITY);
+		t1.setPriority(Thread.MIN_PRIORITY);
+		
+		t1.start();
+		t2.start();
+		
+		System.out.println("main thread end");
+		
+	}
+
+}
